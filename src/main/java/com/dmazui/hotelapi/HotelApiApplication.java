59d6a7d99e8c7249cfbc5c3cdc2267617ee8e324
diogo <dev.diogo@gmail.com>
2024-05-09 16:19:02 -0300

    fst

---
+++ src/main/java/com/dmazui/hotelapi/HotelApiApplication.java
@@ -0,0 +1,25 @@
+package com.dmazui.hotelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+//@EntityScan(basePackages = {"com.dmazui.todoapi.model"})
+public class HotelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApiApplication.class, args);
+	}
+	
+	@GetMapping
+	public String hello() {
+		return "{\"api\": \"hotel\", \"by\" : \"dmazui\"}";
+	}
+
+	
+	
+}
